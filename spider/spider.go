7cb840a3871f7cd38dad9fe91fb8ad8c235a98d5
lck <lance.kidwell@asynchrony.com>
2019-03-06 21:41:13 -0600

    cromunlence

--- spider/spider.go
+++ spider/spider.go
@@ -1,5 +1,15 @@
 package spider
 
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	prose "gopkg.in/jdkato/prose.v2"
+)
+
 // Question is a question
 type Question struct {
 	ID   int    `json:"id"`
@@ -11,5 +21,39 @@ func CreateQuestion(id int, text string) *Question {
 	return &Question{
 		ID:   id,
 		Text: text,
+	}
+}
+
+// Process processes the process
+func Process() error {
+	var counter int
+	csvFile, _ := os.Open("../data/qd-raw.csv")
+	r := csv.NewReader(csvFile)
+	defer func() {
+		log.Println("Done")
+	}()
+	for {
+		record, err := r.Read()
+
+		if err == io.EOF {
+			log.Println("Ran out of text yo")
+			return nil
+		}
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		if counter > 10 {
+			log.Println("OMG slow down")
+			return nil
+		}
+		doc, _ := prose.NewDocument(fmt.Sprintf("<h1>%s</h1> <article>%s</article>", record[0], record[1]))
+		log.Printf("doc: %+v \n\n", doc)
+		for _, ent := range doc.Entities() {
+			fmt.Println(ent.Text, ent.Label)
+			// Lebron James PERSON
+			// Los Angeles GPE
+		}
+		counter++
 	}
 }
--- spider/spider_test.go
+++ spider/spider_test.go
@@ -3,12 +3,18 @@ package spider_test
 import (
 	"testing"
 
+	"github.com/muddylemon/queryd/spider"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_CreateQuestion(t *testing.T) {
-	testQuestion := queryd.CreateQuestion(55, "The Monkey Sings Tonight")
+	testQuestion := spider.CreateQuestion(55, "The Monkey Sings Tonight")
+	//assert.Equal(t, testQuestion.Text, "The Chicken Screams Today")
+	assert.Equal(t, testQuestion.Text, "The Monkey Sings Tonight")
 
-	assert.Equal(t, testQuestion.Text, "The Chicken Screams Today")
+}
 
+func Test_ProcessText(t *testing.T) {
+	results := spider.Process()
+	assert.Nil(t, results)
 }
